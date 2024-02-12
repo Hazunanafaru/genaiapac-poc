2fe3fcbf2b7b47dc59f5e8c8371a2135d58f1a9f
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-02-12 18:35:48 +0700

    feat: add download and check model functions

--- llm/__init__.py
+++ llm/__init__.py
@@ -1,14 +1,22 @@
 from llama_cpp import Llama
 import time
+from llm.model import download_model, check_model
 
-def llm_inference(query: str, model_path: str):
+
+def llm_inference(query: str, model_location: str, model_path: str):
     """
     Inference qury to LLM model
     Take query and model path then compose query to prompt template
     Return tupple of latency and prompt output
     """
+
+    if model_location != "local" and check_model is False:
+        download_model(model_location=model_location, model_path=model_path)
+
     LLM = Llama(model_path=model_path, n_gpu_layers=32, n_threads=6, n_ctx=3584, n_batch=521, verbose=True, temperature=0, cuda_device=0)
-    prompt_template = f"""Kamu adalah seorang asisten AI yang akan membantu dokter dalam mengidentifikasi permasalahan pasien. \n
+
+    prompt_template = f"""
+    Kamu adalah seorang asisten AI yang akan membantu dokter dalam mengidentifikasi permasalahan pasien. \n
     Kamu akan mendengarkan percakapan antara dokter dengan pasiennya, lalu kamu harus melakukan Name Entity Recognition untuk gejala penyakit pasien. \n
     Contohnya: "Batuk", "Mual", "Bersin", dll. \n
     Setelah melakukan Name Entity Recognition, kamu harus memberikan kemungkinan diagnosa penyakit pasien beserta kemungkinan (dalam persentase) dan alasan akan diagnosa tersebut. \n
@@ -26,8 +34,11 @@ def llm_inference(query: str, model_path: str):
     {query}
     ```
     """
+
     before = time.time()
+    # Process LLM Inference
     output = LLM(prompt_template, max_tokens=0)
     after = time.time()
     latency = after - before
+
     return latency, output
---
+++ llm/model.py
@@ -0,0 +1,59 @@
+import os
+from requests import get
+from google.cloud import storage
+
+def download_model(model_location: str, model_path: str) -> str:
+    """
+    Download LLM Model from gcs or huggingface.
+    Take model_location and model_path.
+    For model stored in gcs, use google cloud storage lib to download model.
+    For model stored in huggingface, use requests lib to stream download model.
+    Return str of model name.
+    Example:
+    >>> download_model(model_location="gcs", model_path="gs://genai/model/seallm.gguf")
+    "seallm.gguf"
+    """
+    model_name = model_path.split("/")[-1]
+
+    match model_location:
+        case "gcs":
+            # TODO need to switch to streaming download mode
+            # Ref: https://cloud.google.com/storage/docs/streaming-downloads
+            gcs_bucket = model_path.split("/")[2]
+            gcs_blob = model_path.split(gcs_bucket)[1][1:]
+
+            gcs_client = storage.Client()
+
+            bucket = gcs_client.bucket(gcs_bucket)
+            blob = bucket.blob(gcs_blob)
+            blob.download_to_filename(model_name)
+            return model_name
+        case "huggingface":
+            url = model_path + "?download=true"
+            with get(url, stream=True) as response:
+                response.raise_for_status()
+                with open(model_name, 'wb') as file:
+                    for chunk in response.iter_content(chunk_size=10*1024*1024):
+                        file.write(chunk)
+            return model_name
+        case _:
+            raise Exception("""
+            External model location supported are gcs and huggingface.
+            For gcs, make sure your model_path follow this format:
+            gs://<bucket_name>/<bucket_path>/<model_file_name>
+            For huggingface, make sure your model_path follow this format:
+            https://huggingface.co/<author>/<model_name>/blob/main/<model_file_name>.gguf
+            """)
+
+def check_model(model_path: str) -> bool:
+    """
+    Check model availability in local directory
+    Take model_path from model_location hugginface or gcs.
+    Check the last path of huggingface or gcs endpoint to set the model_name.
+    Then check if the same model_name is available in current directory.
+    Return boolean. True if file exist and False if not.
+    """
+    model_name = model_path.split("/")[-1]
+    if os.path.isfile(os.getcwd() + "/" + model_name):
+        return True
+    return False
