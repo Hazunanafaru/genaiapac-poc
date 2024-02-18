a0e95a54496d6acd5e8aa0708331cb77c0d66569
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-02-18 12:42:29 +0700

    fix: use DEBUG environment variable to dynamically set flask run config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Module providing main initial functionality"""
 import logging
 from dotenv import load_dotenv
+from llama_cpp import os
 from api import init_app
 
 def main():
@@ -13,11 +14,11 @@ def main():
     logging.getLogger().setLevel(logging.WARNING)
     app = init_app()
 
-    if app.config['DEBUG']:
+    if os.environ.get('DEBUG'):
         logging.getLogger().setLevel(logging.DEBUG)
-        app.run(debug=True)
-
-    app.run(host="0.0.0.0", debug=False)
+        app.run(debug=True, port=5000)
+    else:
+        app.run(host="0.0.0.0", debug=False, port=8080)
 
 if __name__ == '__main__':
     main()
